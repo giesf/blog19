954718459d0ded4454525d40a8ba72235af302df
giesf <64958447+giesf@users.noreply.github.com>
2024-01-18 11:26:19 +0000

    Added overwrite-css command

--- src/cli/commands.ts
+++ src/cli/commands.ts
@@ -3,7 +3,8 @@ import { initTables } from '../data/tables'
 import { startServer } from '../server'
 import { initDB } from '../services/db'
 import { startWizard } from './wizard'
-
+import { cpSync } from 'fs'
+import { join } from 'path'
 export type CLICommand = {
     usage: string,
     description?: string,
@@ -28,6 +29,22 @@ export const commands: {
         "description": "Start the configuration wizard",
         "handler": () => {
             startWizard()
+        }
+    },
+    "overwrite-css": {
+        "usage": "overwrite-css",
+        "description": "Copy the default css file to static directory",
+        "handler": () => {
+            const applicationSupportDir = join(import.meta.dir, "../..")
+            const cwd = process.cwd()
+            if (cwd == applicationSupportDir) {
+                console.error("You are in a development environment this ain't gonna work")
+                process.exit(1)
+                return;
+            }
+            console.log("Overwriting css...")
+            cpSync(join(import.meta.dir, "../../static/base.css"), "./static/base.css", { recursive: true })
+
         }
     }
 }
